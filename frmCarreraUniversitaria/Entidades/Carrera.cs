4328544b021a622b60d071d4418aa2101e03310b
Mateo Moszoro <Mateo@MATEO-PC>
2023-08-29 20:35:33 -0300

    Eliminacion de Override ToString

--- frmCarreraUniversitaria/Entidades/Carrera.cs
+++ frmCarreraUniversitaria/Entidades/Carrera.cs
@@ -45,10 +45,6 @@ namespace frmCarreraUniversitaria.Entidades
             Detalles.RemoveAt(posicion);
         }
 
-        public override string ToString()
-        {
-            return base.ToString();
-        }
 
     }
 }
